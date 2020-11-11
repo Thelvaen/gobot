dfafd340918ccc5e5f8166e580ca6ce493ecc85f
Thelvaen Mandel <thelvaen.mandel@gmail.com>
2020-11-11 19:53:09 +0100

    update dice command

--- main.go
+++ main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"fmt"
-
+	"math/rand"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
 
 	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/spf13/viper"
 )
 
 var (
-	err       error
-	client    *twitch.Client
-	messages  []string
-	position  int
-	stackSize int
-	channels  []string
-	tokenDef  bool
+	err          error
+	twitchC      *twitch.Client
+	messages     []string
+	position     int
+	stackSize    int
+	mainChan     string
+	channels     []string
+	tokenDef     bool
+	randomSource *rand.Rand
 
 	url  string
 	port string
@@ -30,8 +36,11 @@ func init() {
 		panic(fmt.Errorf("fatal error config file: %s", err))
 	}
 
+	randomSource = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 	stackSize = viper.GetInt("StackSize")
 	channels = viper.GetStringSlice("AgregChans")
+	mainChan = viper.GetString("MainChannel")
 
 	messages = make([]string, stackSize+10)
 	position = 0
@@ -47,6 +56,20 @@ func init() {
 
 	// Initializing routes
 	http.HandleFunc("/messages", getMessages)
+}
+
+func isInt(s string) bool {
+	for _, c := range s {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func rollDice(userName string, faces int) {
+	// Rolling a dice
+	twitchC.Say(mainChan, fmt.Sprintf("* Lance un dé à %d faces pour %s et obtient : %d", faces, userName, randomSource.Intn(faces)+1))
 }
 
 func pushMessage(data string) {
@@ -68,11 +91,31 @@ func getMessages(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(w, channel)
 		fmt.Fprintf(w, " ")
 	}
+	fmt.Fprintf(w, mainChan)
 	fmt.Fprintf(w, "</h1>")
 	for i := 0; i < position; i++ {
 		fmt.Fprintf(w, "<li>%s</li>\n", messages[i])
 	}
 	fmt.Fprintf(w, "</ul></body></html>")
+}
+
+func parseMessage(message twitch.PrivateMessage) {
+	if (message.Channel == mainChan) && strings.HasPrefix(message.Message, "!") {
+		// Command to process
+		command := strings.Fields(message.Message)
+		switch command[0] {
+		case "!dice":
+			if len(command) > 1 && isInt(command[1]) {
+				// Dice faces
+				faces, _ := strconv.Atoi(command[1])
+				rollDice(message.User.Name, faces)
+			} else {
+				rollDice(message.User.Name, 10)
+			}
+		}
+	}
+	pushMessage(fmt.Sprintf("#%s [%02d:%02d:%02d] &lt;%s&gt; %s", message.Channel, message.Time.Hour(), message.Time.Minute(), message.Time.Second(), message.User.Name, message.Message))
+
 }
 
 func main() {
@@ -81,21 +124,24 @@ func main() {
 
 	// Connecting to Twitch
 	if viper.IsSet("Credential.Nickname") && viper.IsSet("Credential.Token") {
-		client = twitch.NewClient(viper.GetString("Credential.Nickname"), viper.GetString("Credential.Token"))
+		twitchC = twitch.NewClient(viper.GetString("Credential.Nickname"), viper.GetString("Credential.Token"))
+		//fmt.Println("Authenticated connection")
 	} else {
 		// No credentials provided, anon connection
-		client = twitch.NewAnonymousClient()
+		twitchC = twitch.NewAnonymousClient()
+		//fmt.Println("Anonymous connection")
 	}
 
-	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-		pushMessage(fmt.Sprintf("#%s [%02d:%02d:%02d] &lt;%s&gt; %s", message.Channel, message.Time.Hour(), message.Time.Minute(), message.Time.Second(), message.User.Name, message.Message))
+	twitchC.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		parseMessage(message)
 	})
 
+	twitchC.Join(mainChan)
 	for _, channel := range channels {
-		client.Join(channel)
+		twitchC.Join(channel)
 	}
 
-	err = client.Connect()
+	err = twitchC.Connect()
 	if err != nil {
 		panic(fmt.Errorf("can't connect to Twitch: %s", err))
 	}
