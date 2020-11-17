a656661e1eff886dbe040c0d122c9345a8a02b70
Thelvaen Mandel <thelvaen.mandel@gmail.com>
2020-11-17 01:05:23 +0100

    updates

--- assets.go
+++ assets.go
@@ -6,25 +6,15 @@ import (
 	"github.com/jessevdk/go-assets"
 )
 
-var _Assets792d102622f6e891107803cd74fc7253e1d8f61f = "{{define \"content\"}}\r\n    <h1>Statistiques pour {{ .MainChannel }}</h1>\r\n    <table class=\"table table-bordered\">\r\n        <thead>\r\n            <tr>\r\n                <th scope=\"col\">User</th>\r\n                <th scope=\"col\">Score</th>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n    {{range $user, $score := .Data.Statistiques}}\r\n            <tr>\r\n                <td>{{ $user }}</td>\r\n                <td>{{ $score }}</td>\r\n            </tr>\r\n    {{end}}\r\n        </tbody>\r\n    </table>\r\n{{end}}\r\n"
-var _Assets9c21febaee572857d6d7f084f3f6b603538796a3 = "<!DOCTYPE html>\r\n<html>\r\n\r\n<head>\r\n    <title>Twitch bot - {{ .MainChannel }}</title>\r\n    <!-- CSS -->\r\n    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css\" integrity=\"sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2\" crossorigin=\"anonymous\">\r\n\r\n    <!-- jQuery and JS bundle w/ Popper.js -->\r\n    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\r\n    <!-- <script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script> -->\r\n    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx\" crossorigin=\"anonymous\"></script>\r\n</head>\r\n\r\n<body>\r\n    <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n        <a class=\"navbar-brand\" href=\"#\">Fonctions du Bot</a>\r\n        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n            <span class=\"navbar-toggler-icon\"></span>\r\n        </button>\r\n        <div class=\"collapse navbar-collapse\" id=\"navbarNav\">\r\n            <ul class=\"navbar-nav\">\r\n            {{ range $route, $routeDetails := .WebRoutes }}\r\n            <li class=\"nav-item\"><a class=\"nav-link\" href=\"{{ $route }}\">{{ $routeDetails.RouteDesc }}</a></li>\r\n            {{ end }}\r\n            </ul>\r\n        </div>\r\n    </nav>\r\n    {{template \"content\" .}}\r\n</body>\r\n</html>"
-var _Assetsa9531d392a275cf19c734adc26dcc55447e7419e = "{{define \"content\"}}\r\n    <h1>\r\n    {{range $index, $channel := .Channels}}\r\n        {{ $channel }}\r\n    {{end}}\r\n    {{ .MainChannel }}\r\n    </h1>\r\n        <table id=\"messages\">\r\n        <thead>\r\n            <tr>\r\n                <th data-field=\"Channel\">Channel</th>\r\n                <th data-field=\"Time\">Time</th>\r\n                <th data-field=\"User.DisplayName\">User</th>\r\n                <th data-field=\"Message\">Message</th>\r\n            </tr>\r\n        </thead>\r\n    </table>\r\n    <script src=\"https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js\"></script>\r\n    <script src=\"https://unpkg.com/bootstrap-table@1.18.0/dist/extensions/auto-refresh/bootstrap-table-auto-refresh.min.js\"></script>\r\n    <script>\r\n$(document).ready(function(){\r\n    getMyData();\r\n//    setInterval( getMyData, 5000 );\r\n\r\n    setTimeout(function(){\r\n        window.location.reload(1);\r\n    }, 5000);\r\n});\r\n\r\n\r\nfunction getMyData() {\r\n    return $.getJSON( \"http://localhost:8080/json/messages\", function( messages ) {\r\n        $('#messages').bootstrapTable({\r\n            data: messages\r\n        });\r\n    });\r\n}\r\n    </script>\r\n{{end}}\r\n"
+var _Assetsa9531d392a275cf19c734adc26dcc55447e7419e = "{{define \"content\"}}\r\n    <h1>\r\n    {{range $index, $channel := .Channels}}\r\n        {{ $channel }}\r\n    {{end}}\r\n    {{ .MainChannel }}\r\n    </h1>\r\n        <table id=\"messages\" data-url=\"http://localhost:8080/json/messages\" data-auto-refresh=\"true\" data-auto-refresh-interval=\"5\">\r\n        <thead>\r\n            <tr>\r\n                <th data-field=\"Channel\">Channel</th>\r\n                <th data-field=\"Time\" data-formatter=\"timeFormater\">Time</th>\r\n                <th data-field=\"User.DisplayName\">User</th>\r\n                <th data-field=\"Message\">Message</th>\r\n            </tr>\r\n        </thead>\r\n    </table>\r\n    <script src=\"https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js\"></script>\r\n    <script src=\"https://unpkg.com/bootstrap-table@1.18.0/dist/extensions/auto-refresh/bootstrap-table-auto-refresh.min.js\"></script>\r\n    <script>\r\n    $(document).ready(function(){\r\n        $('#messages').bootstrapTable();\r\n    });\r\n\r\n    /*function chanFormater(value) {\r\n        return '#' + value\r\n    }\r\n\r\n    function userFormater(value) {\r\n        return '&lt;' + value + '&gt;'\r\n    }*/\r\n\r\n    function timeFormater(value) {\r\n        var time = new Date(value);\r\n        h = time.getHours();\r\n        m = time.getMinutes();\r\n        s = time.getSeconds();\r\n\r\n        if (h < 10) {\r\n            h = '0' + h\r\n        }\r\n        if (m < 10) {\r\n            m = '0' + m\r\n        }\r\n        if (s < 10) {\r\n            s = '0' + s\r\n        }\r\n        return h + ':' + m + ':' + s\r\n    }\r\n\r\n    </script>\r\n{{end}}\r\n"
 var _Assetsee7a22cac18b698ae6c74dc1bfe05f4a92a61e87 = "Error {{ .error }}"
 var _Assets72baf2332493c53d1630406a465d95d6e729a9b1 = "{{define \"content\"}}\r\n<div class=\"form-bottom\">\r\n    <form role=\"form\" action=\"\" method=\"post\" class=\"login-form\">\r\n        <div class=\"form-group\">\r\n            <label for=\"form-username\">Nom d'utilisateur</label>\r\n            <input name=\"username\" placeholder=\"Username...\" class=\"form-username form-control\" id=\"form-username\" type=\"text\" required>\r\n        </div>\r\n        <div class=\"form-group\">\r\n            <label for=\"form-password\">Mot de passe</label>\r\n            <input name=\"password\" placeholder=\"Password...\" class=\"form-password form-control\" id=\"form-password\" type=\"password\" required>\r\n        </div>\r\n        <button type=\"submit\" class=\"btn btn-primary\">Se connecter</button>\r\n        <input type=\"hidden\" name=\"_csrf\" value=\"{{ .CSRF_Token }}\" />\r\n    </form>\r\n</div>\r\n{{end}}"
+var _Assets792d102622f6e891107803cd74fc7253e1d8f61f = "{{define \"content\"}}\r\n    <h1>Statistiques pour {{ .MainChannel }}</h1>\r\n    <table class=\"table table-bordered\">\r\n        <thead>\r\n            <tr>\r\n                <th scope=\"col\">User</th>\r\n                <th scope=\"col\">Score</th>\r\n            </tr>\r\n        </thead>\r\n        <tbody>\r\n    {{range $user, $score := .Data.Statistiques}}\r\n            <tr>\r\n                <td>{{ $user }}</td>\r\n                <td>{{ $score }}</td>\r\n            </tr>\r\n    {{end}}\r\n        </tbody>\r\n    </table>\r\n{{end}}\r\n"
+var _Assets9c21febaee572857d6d7f084f3f6b603538796a3 = "<!DOCTYPE html>\r\n<html>\r\n\r\n<head>\r\n    <title>Twitch bot - {{ .MainChannel }}</title>\r\n    <!-- CSS -->\r\n    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css\" integrity=\"sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2\" crossorigin=\"anonymous\">\r\n\r\n    <!-- jQuery and JS bundle w/ Popper.js -->\r\n    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\r\n    <!-- <script src=\"https://code.jquery.com/jquery-3.5.1.slim.min.js\" integrity=\"sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj\" crossorigin=\"anonymous\"></script> -->\r\n    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx\" crossorigin=\"anonymous\"></script>\r\n</head>\r\n\r\n<body>\r\n    <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n        <a class=\"navbar-brand\" href=\"#\">Fonctions du Bot</a>\r\n        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n            <span class=\"navbar-toggler-icon\"></span>\r\n        </button>\r\n        <div class=\"collapse navbar-collapse\" id=\"navbarNav\">\r\n            <ul class=\"navbar-nav\">\r\n            {{ range $route, $routeDetails := .WebRoutes }}\r\n            <li class=\"nav-item\"><a class=\"nav-link\" href=\"{{ $route }}\">{{ $routeDetails.RouteDesc }}</a></li>\r\n            {{ end }}\r\n            </ul>\r\n        </div>\r\n    </nav>\r\n    {{template \"content\" .}}\r\n</body>\r\n</html>"
 
 // Assets returns go-assets FileSystem
 var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"includes", "layouts"}, "/includes": []string{"aggregator.html", "error.html", "login_form.html", "stats.html"}, "/layouts": []string{"index.html"}}, map[string]*assets.File{
-	"/layouts/index.html": &assets.File{
-		Path:     "/layouts/index.html",
-		FileMode: 0x1b6,
-		Mtime:    time.Unix(1605540008, 1605540008831828400),
-		Data:     []byte(_Assets9c21febaee572857d6d7f084f3f6b603538796a3),
-	}, "/": &assets.File{
-		Path:     "/",
-		FileMode: 0x800001ff,
-		Mtime:    time.Unix(1605553836, 1605553836077530200),
-		Data:     nil,
-	}, "/includes": &assets.File{
+	"/includes": &assets.File{
 		Path:     "/includes",
 		FileMode: 0x800001ff,
 		Mtime:    time.Unix(1605481166, 1605481166764571800),
@@ -32,7 +22,7 @@ var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"includes", 
 	}, "/includes/aggregator.html": &assets.File{
 		Path:     "/includes/aggregator.html",
 		FileMode: 0x1b6,
-		Mtime:    time.Unix(1605546165, 1605546165000266100),
+		Mtime:    time.Unix(1605570365, 1605570365386677900),
 		Data:     []byte(_Assetsa9531d392a275cf19c734adc26dcc55447e7419e),
 	}, "/includes/error.html": &assets.File{
 		Path:     "/includes/error.html",
@@ -53,5 +43,15 @@ var Assets = assets.NewFileSystem(map[string][]string{"/": []string{"includes", 
 		Path:     "/layouts",
 		FileMode: 0x800001ff,
 		Mtime:    time.Unix(1605392048, 1605392048179565000),
+		Data:     nil,
+	}, "/layouts/index.html": &assets.File{
+		Path:     "/layouts/index.html",
+		FileMode: 0x1b6,
+		Mtime:    time.Unix(1605540008, 1605540008831828400),
+		Data:     []byte(_Assets9c21febaee572857d6d7f084f3f6b603538796a3),
+	}, "/": &assets.File{
+		Path:     "/",
+		FileMode: 0x800001ff,
+		Mtime:    time.Unix(1605570310, 1605570310164156700),
 		Data:     nil,
 	}}, "")
